4471860128f227e6115aeaaf600cd289c4f5800a
NurMukashev <mukashev.nur@gmail.com>
2024-03-27 11:00:05 +0500

    Index и Create разметка

---
+++ resources/js/Pages/Posts/Create.vue
@@ -0,0 +1,42 @@
+<template>
+    <Head title="Создание поста" />
+
+    <h2>Создание поста</h2>
+    <Link :href="route('posts.index')">назад</Link>
+
+    <form @submit.prevent="submit">
+        <input v-model="title" type="text" placeholder="Title" />
+        <div v-if="errors.name">{{ errors.name }}</div>
+
+        <textarea v-model="text" id="" cols="30" rows="10"></textarea>
+        <div v-if="errors.text">{{ errors.text }}</div>
+
+        <button type="submit" >создать</button>
+    </form>
+</template>
+
+<script>
+import {Head, Link} from "@inertiajs/vue3";
+export default {
+    name: "Create",
+
+    components: {
+        Head,
+        Link,
+    },
+
+    props: {
+        errors: Object,
+    },
+
+    methods: {
+        submit(){
+            alert('123');
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,49 @@
+<template>
+    <Head title="Все посты" />
+    <h2>Все посты</h2>
+
+    <Link :href="route('posts.create')">создать</Link>
+    <table>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>категория</th>
+                <th>автор</th>
+                <th>title</th>
+                <th>text</th>
+                <th>создано</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="post in posts">
+                <td>{{ post.id }}</td>
+                <td>{{ post.id }}</td>
+                <td>{{ post.id }}</td>
+                <td>{{ post.title }}</td>
+                <td>{{ post.text }}</td>
+                <td>{{ post.created_at }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import {Head, Link} from "@inertiajs/vue3";
+
+export default {
+    name: "Index",
+
+    components: {
+        Head,
+        Link
+    },
+
+    props:{
+        posts: Object,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
