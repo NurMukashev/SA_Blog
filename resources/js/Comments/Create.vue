2b5243a1d111e1d485fe58062537bfa9166254bb
NurMukashev <mukashev.nur@gmail.com>
2024-03-28 20:31:43 +0500

    Наброски для комментариев

---
+++ resources/js/Comments/Create.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {router, useForm} from "@inertiajs/vue3";
+
+    const form = useForm({
+        newComment: ''
+    })
+
+    function store(){
+        router.post('/comments', {
+            text: form.newComment,
+        })
+    }
+
+</script>
+
+<template>
+
+    <form @submit.prevent="store">
+        <textarea v-model="form.newComment"></textarea>
+        <button type="submit">добавить</button>
+    </form>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Comments/Index.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+const props = defineProps({
+    comments: Object
+})
+
+</script>
+
+<template>
+    <div v-for="comment in comments" :key="comment.id">
+        <p>{{ comment.text }}</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
